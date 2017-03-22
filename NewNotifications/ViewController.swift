fffb54fdf47eb91c6dd8a1631e245d4471b89bde
Gagandeeps-Air <gagandeepsingh@Gagandeeps-MacBook-Air.local>
2017-03-22 20:08:42 +0530

    Demonstrate simple local notification

--- NewNotifications/ViewController.swift
+++ NewNotifications/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Request notification permission
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.badge, .sound] , completionHandler: { (granted, error) in
+            
+            if granted {
+                print("Notification access granted")
+            }else {
+                print(error?.localizedDescription)
+            }
+            
+        })
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func notifyButtonTapped(_ sender: UIButton) {
+        
+        scheduleNotification(inSeconds: 5, completion: {success in
+        
+            if success {
+                print("Succesfully scheduled notification")
+            }else {
+                print("Error scheduling notification")
+            }
+        })
+        
     }
-
+    
+    //Function to sechedule notification
+    func scheduleNotification(inSeconds: TimeInterval, completion: @escaping (Bool) -> ()) {
+        
+        let notif = UNMutableNotificationContent()
+        
+        notif.title = "New Notification"
+        notif.subtitle = "These are great"
+        notif.body = "The new notification in the IOS 10  are what I've always dreamed of!"
+        
+        let notifTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        
+        
+        
+        let request = UNNotificationRequest(identifier: "myNotification", content: notif, trigger: notifTrigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            
+            if error != nil {
+                print(error)
+                completion(false)
+            }else {
+                completion(true)
+            }
+        })
+    }
 
 }
 
